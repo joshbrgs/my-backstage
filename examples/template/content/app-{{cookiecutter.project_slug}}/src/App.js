1a721cff434084817f467c11dd5743feeffdd8b9
Josh Burgess <burgessj247@gmail.com>
2025-02-11 15:26:54 -0500

    Adds cookiecutter template

---
+++ examples/template/content/app-{{cookiecutter.project_slug}}/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello {{cookiecutter.github_username}}! Welcome to your new React app {{cookiecutter.project_slug}}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
